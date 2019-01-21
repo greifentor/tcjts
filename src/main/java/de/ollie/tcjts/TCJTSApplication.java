cc798bd014ebb75cea4dc8523f976e376fbe0e29
ollie <lieshoff@gmx.de>
2019-01-21 20:02:41 +0100

    First project files.

---
+++ src/main/java/de/ollie/tcjts/TCJTSApplication.java
@@ -0,0 +1,14 @@
+package de.ollie.tcjts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcjtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcjtsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/de/ollie/tcjts/TcjtsApplicationTests.java
@@ -0,0 +1,17 @@
+package de.ollie.tcjts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcjtsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
